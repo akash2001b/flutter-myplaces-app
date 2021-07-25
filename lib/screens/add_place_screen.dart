487e12cf4fc50756ba2ce00d6a41fdd20fdfef9e
akash Roy Choudhury <arc.ar602@gmail.com>
2021-07-25 19:07:20 +0530

    project setup

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  // const AddPlaceScreen({ Key? key }) : super(key: key);
+  static const routeName = '/add-place';
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        children: [
+          Text('User Inputs.....'),
+          // Raise
+        ],
+      ),
+    );
+  }
+}
