b6a428f5ce39a61fdb15388f7b9a02fe2a69c62a
akash Roy Choudhury <arc.ar602@gmail.com>
2021-07-25 20:28:58 +0530

    pick image added

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import '../widget/image_input.dart';
+import 'dart:io';
+import 'package:provider/provider.dart';
+import '../providers/great_places.dart';
 class AddPlaceScreen extends StatefulWidget {
   // const AddPlaceScreen({ Key? key }) : super(key: key);
   static const routeName = '/add-place';
@@ -8,6 +11,21 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController=TextEditingController();
+  File _pickedImage;
+  void _selectImage(File pickedImage){
+    _pickedImage=pickedImage;
+  }
+
+  void _savePlace(){
+    if(_titleController.text.isEmpty || _pickedImage==null){
+      return;
+    }
+    Provider.of<GreatPlaces>(context,listen: false).addPlace(_titleController.text, _pickedImage);
+
+    Navigator.of(context).pop();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +33,35 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text('Add a New Place'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('User Inputs.....'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            onPressed: _savePlace,
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          ),
           // Raise
         ],
       ),
